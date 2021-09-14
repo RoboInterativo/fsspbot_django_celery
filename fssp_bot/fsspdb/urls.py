be13962162bc3e844b1a291a5905fff6918f5b48
chiefexb <chief_exb@mail.ru>
2021-09-15 01:36:00 +0300

    1

--- fssp_bot/fsspdb/urls.py
+++ fssp_bot/fsspdb/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name='index'),
     path('webhook_status', views.webhook_status, name='webhook_status'),
+    path('delete_webhook', views.delete_webhook, name='delete_webhook'),
+    path('set_webhook', views.set_webhook, name='set_webhook'),
     #path("HOOK-SyBPwfCLPl30", views.webhook, name='webhook'),
     #path("<slug:slug>", views.webhook, name='webhook'),
     #123
--- fssp_bot/fsspdb/views.py
+++ fssp_bot/fsspdb/views.py
@@ -13,10 +13,25 @@ import yaml
 with open('/home/django/config.yml') as f:
     config=yaml.safe_load(f)
 
-TELEGRAM_TOKEN= config.get('TELEGRAM_TOKEN')
+#TELEGRAM_TOKEN= config.get('TELEGRAM_TOKEN')
 
+def set_webhook(request):
+    TELEGRAM_TOKEN=request.get('token')
+    method='setWebhook'
+    url=f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/{method}'
+    tel_url="https://174.138.1.184/webhook"
+    r=requests.get(url,{'url':tel_url})
+    return HttpResponse(r.text )
+
+def delete_webhook(request):
+    TELEGRAM_TOKEN=request.get('token')
+    method='deleteWebhook'
+    url=f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/{method}'
+    r=requests.get(url)
+    return HttpResponse(r.text )
 
 def webhook_status(request):
+    TELEGRAM_TOKEN=request.get('token')
     method='getWebhookInfo'
     url=f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/{method}'
     r=requests.get(url)
