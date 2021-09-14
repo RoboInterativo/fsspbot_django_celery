101de1359f8768ee6cd44afdaa4f87f7052df05b
chiefexb <chief_exb@mail.ru>
2021-09-14 15:10:57 +0300

    1

--- fssp_bot/fsspdb/views.py
+++ fssp_bot/fsspdb/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index (request):
+
+    # p = Food.objects.all()
+    # now = datetime.datetime.now()
+    # t = get_template('index.html')
+    # html = t.render(context={'food':p}, request=None)
+    html="OK"
+    return HttpResponse(html)
